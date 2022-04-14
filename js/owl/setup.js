ddbf7087049515ec9196db7bfce029b70da9b7fa
Kadu1811 <kadu.dear60@gmail.com>
2022-04-14 19:29:22 -0300

    editing responsivity of carousel

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -4,13 +4,16 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     responsive:{
         0:{
-            items:1
+            items:3
         },
         600:{
             items:4
         },
         1000:{
             items:5
+        },
+        1200:{
+            items:7
         }
     }
 })
